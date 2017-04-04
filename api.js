7477fe7ea0535e895361632829204595e8a5d293
Anthony Gore <anthony@magnum.technology>
2017-04-04 20:04:07 +0700

    Updating API data

--- api.js
+++ api.js
@@ -12,14 +12,15 @@ const $http = axios.create({
 
 function generateSessions(id) {
   let sessions = [];
-  let nums = id.replace('tt', '').split('').map(item => parseInt(item));
+  let nums = id.replace('tt', '').split('').map(item => parseInt(item)).reverse();
   nums.forEach((num, index) => {
     let date = moment().startOf('day').add(index, 'days');
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i <= num; i++) {
       let pos = index + i <= nums.length ? index + i : index + i - nums.length;
       let hours = nums[pos] + 12;
       let mins = nums[pos] < 2.5 ? 0 : nums[pos] < 5 ? 15 : nums[pos] < 7.5 ? 30 : 45;
       sessions.push({
+        id: `${id}_${i}`,
         time: moment(date).add(hours, 'hours').add(mins, 'minutes'),
         seats: Math.round(200 - nums.reduce((acc, val) => { return acc + val; }) + (num * i))
       });
@@ -28,45 +29,58 @@ function generateSessions(id) {
   return sessions.sort((a, b) => { if (a.time < b.time) { return - 1 } else { return a.time > b.time; } });
 }
 
+function cleanData(movie) {
+  if (movie.Rated === 'N/A' || movie.Rated === 'UNRATED' || movie.Rated === 'NOT RATED') {
+    let last = parseInt(movie.imdbID[movie.imdbID.length - 1]);
+    movie.Rated = last < 7 ? ( last < 4 ? 'G' : 'PG-13' ) : 'R';
+  }
+  return movie;
+}
+
 module.exports = {
+  data: [],
   getData(callback) {
-    let ids = process.env.IMDB_IDS.split(',');
-    let data = [];
-    async.each(
-      ids,
-      function (id, callback) {
-        if (!data.find(item => item.id === id)) {
-          $http.get(`?i=${id}`)
-            .then(
-              function (response) {
-                if (!response.data.Error) {
-                  data.push({
-                    id,
-                    movie: response.data,
-                    sessions: generateSessions(id)
-                  });
-                  data.sort((a, b) => { if (a.id < b.id) { return - 1 } else { return a.id > b.id; } });
-                } else {
-                  console.log(response.data.Error);
+    if (!this.data.length) {
+      let ids = process.env.IMDB_IDS.split(',');
+      let data = [];
+      async.each(
+        ids,
+        function (id, callback) {
+          if (!data.find(item => item.id === id)) {
+            $http.get(`?i=${id}`)
+              .then(
+                function (response) {
+                  if (!response.data.Error) {
+                    data.push({
+                      id,
+                      movie: cleanData(response.data),
+                      sessions: generateSessions(id)
+                    });
+                  } else {
+                    console.log(response.data.Error);
+                  }
+                  callback();
+                },
+                function (err) {
+                  callback(err);
                 }
-                callback();
-              },
-              function (err) {
-                callback(err);
-              }
-            )
-          ;
-        } else {
-          callback();
+              )
+            ;
+          } else {
+            callback();
+          }
+        },
+        (err) => {
+          if (err) {
+            callback(err, null);
+          } else {
+            this.data = ids.map(id => data.find(item => id === item.id) );
+            callback(null, this.data);
+          }
         }
-      },
-      function(err) {
-        if (err) {
-          callback(err, null);
-        } else {
-          callback(null, data);
-        }
-      }
-    );
+      );
+    } else {
+      callback(null, this.data);
+    }
   }
 };
