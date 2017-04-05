8317571b8c2f7e5906f94a6fdc2c0a3c794e04ee
Anthony Gore <anthony@magnum.technology>
2017-04-05 16:39:36 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -29,12 +29,16 @@ new Vue({
       template: `<div id="movie-filter">
                     <h2>Filter results</h2>
                     <div class="filter-group">
-                      <check-filter v-for="genre in genres"></check-filter>                   
+                      <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>                   
                     </div>                 
                 </div>`,
       components: {
         'check-filter': {
-          template: `<div>Filter</div>`
+          props: [ 'title' ],
+          template: `<div class="check-filter">
+                        <span class="checkbox"></span>
+                        <span class="check-filter-title">{{ title }}</span>
+                    </div>`
         }
       }
     }
