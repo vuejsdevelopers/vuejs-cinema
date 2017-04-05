7b4baee14ecf453b26dd783ebf1197545fd163e6
Anthony Gore <anthony@magnum.technology>
2017-04-05 16:47:41 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -34,8 +34,13 @@ new Vue({
                 </div>`,
       components: {
         'check-filter': {
+          data() {
+            return {
+              checked: false
+            }
+          },
           props: [ 'title' ],
-          template: `<div class="check-filter">
+          template: `<div v-bind:class="{ 'check-filter': true, active: checked }" v-on:click="checked = !checked">
                         <span class="checkbox"></span>
                         <span class="check-filter-title">{{ title }}</span>
                     </div>`
