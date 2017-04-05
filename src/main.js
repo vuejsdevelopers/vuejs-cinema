6b8bc89de28e0fc76fc8a27404fb31bc966b3298
Anthony Gore <anthony@magnum.technology>
2017-04-05 14:20:01 +0700

    Part 2

--- src/main.js
+++ src/main.js
@@ -6,19 +6,21 @@ new Vue({
   components: {
     'movie-list': {
       template: `<div id="movie-list">
-                    <div v-for="movie in movies">{{ movie }}</div>
+                    <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
                 </div>`,
-      data: {
-        movies: [
-          { title: 'Pulp Fiction' },
-          { title: 'Home Alone' },
-          { title: 'Austin Powers' }
-        ]
+      data() {
+        return {
+          movies: [
+            { title: 'Pulp Fiction' },
+            { title: 'Home Alone' },
+            { title: 'Austin Powers' }
+          ]
+        }
       }
     },
     'movie-filter': {
       template: `<div id="movie-filter">
-                    Movie filter
+                    <h2>Filter results</h2>
                 </div>`
     }
   }
