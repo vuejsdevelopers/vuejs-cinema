e31283c65c131bf9e79d17bb338060a2daeb6aac
Anthony Gore <anthony@magnum.technology>
2017-04-06 16:48:56 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -5,9 +5,20 @@ import genres from './util/genres';
 
 new Vue({
   el: '#app',
+  data: {
+    genre: [],
+    time: []
+  },
   methods: {
     checkFilter(category, title, checked) {
-      console.log(category, title, checked);
+      if (checked) {
+        this[category].push(title);
+      } else {
+        let index = this[category].indexOf(title);
+        if (index > -1) {
+          this[category].splice(index, 1);
+        }
+      }
     }
   },
   components: {
