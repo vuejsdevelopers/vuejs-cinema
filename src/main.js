3b2c46a4e9973b5c0d2bd20c5fc61db06bb5595a
Anthony Gore <anthony@magnum.technology>
2017-04-06 16:38:07 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import genres from './util/genres';
 
 new Vue({
   el: '#app',
+  methods: {
+    checkFilter(category, title, checked) {
+      console.log(category, title, checked);
+    }
+  },
   components: {
     'movie-list': {
       template: `<div id="movie-list">
@@ -33,8 +38,8 @@ new Vue({
                     </div>                 
                 </div>`,
       methods: {
-        checkFilter() {
-          console.log('checkFilter');
+        checkFilter(category, title, checked) {
+          this.$emit('check-filter', category, title, checked);
         }
       },
       components: {
@@ -52,7 +57,7 @@ new Vue({
           methods: {
             checkFilter() {
               this.checked = !this.checked;
-              this.$emit('check-filter');
+              this.$emit('check-filter', 'genre', this.title, this.checked);
             }
           }
         }
