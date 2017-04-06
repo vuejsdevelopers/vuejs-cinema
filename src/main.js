a2c979e0942da73ca9652b1f82abf0b15d69046a
Anthony Gore <anthony@magnum.technology>
2017-04-06 17:26:15 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ new Vue({
   components: {
     'movie-list': {
       template: `<div id="movie-list">
-                    <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                    <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
                 </div>`,
       data() {
         return {
@@ -35,7 +35,21 @@ new Vue({
           ]
         }
       },
-      props: [ 'genre', 'time' ]
+      props: [ 'genre', 'time' ],
+      methods: {
+        moviePassesGenreFilter(movie) {
+          if (!this.genre.length) {
+            return true;
+          } else {
+            return this.genre.find(genre => movie.genre === genre);
+          }
+        }
+      },
+      computed: {
+        filteredMovies() {
+          return this.movies.filter(this.moviePassesGenreFilter);
+        }
+      }
     },
     'movie-filter': {
       data() {
