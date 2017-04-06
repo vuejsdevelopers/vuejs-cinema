6d53207657e24be1d2e40a5f679cec1eed4d90ff
Anthony Gore <anthony@magnum.technology>
2017-04-06 15:46:43 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -29,9 +29,14 @@ new Vue({
       template: `<div id="movie-filter">
                     <h2>Filter results</h2>
                     <div class="filter-group">
-                      <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>                   
+                      <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>                   
                     </div>                 
                 </div>`,
+      methods: {
+        checkFilter() {
+          console.log('checkFilter');
+        }
+      },
       components: {
         'check-filter': {
           data() {
@@ -40,10 +45,16 @@ new Vue({
             }
           },
           props: [ 'title' ],
-          template: `<div v-bind:class="{ 'check-filter': true, active: checked }" v-on:click="checked = !checked">
+          template: `<div v-bind:class="{ 'check-filter': true, active: checked }" v-on:click="checkFilter">
                         <span class="checkbox"></span>
                         <span class="check-filter-title">{{ title }}</span>
-                    </div>`
+                    </div>`,
+          methods: {
+            checkFilter() {
+              this.checked = !this.checked;
+              this.$emit('check-filter');
+            }
+          }
         }
       }
     }
