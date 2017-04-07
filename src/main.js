214c796c07fb9c431a1c1be733ce35991a52d8c7
Anthony Gore <anthony@magnum.technology>
2017-04-07 16:51:12 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import './style.scss';
 import MovieList from './components/MovieList.vue';
 import MovieFilter from './components/MovieFilter.vue';
 
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
 new Vue({
   el: '#app',
   data: {
@@ -25,5 +28,8 @@ new Vue({
   components: {
     MovieList,
     MovieFilter
+  },
+  created() {
+    console.log(this.$http);
   }
 });
