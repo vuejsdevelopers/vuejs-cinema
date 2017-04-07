98448227a6c917f023a7ddecc5730793ea5b6b48
Anthony Gore <anthony@magnum.technology>
2017-04-07 17:26:26 +0700

    no message

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,22 +1,13 @@
 <template>
     <div id="movie-list">
-        <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+        <div v-for="movie in filteredMovies" class="movie">{{ movie.movie.Title }}</div>
     </div>
 </template>
 <script>
     import genres from '../util/genres';
 
     export default {
-        data() {
-            return {
-                movies: [
-                    { title: 'Pulp Fiction', genre: genres.CRIME },
-                    { title: 'Home Alone', genre: genres.COMEDY },
-                    { title: 'Austin Powers', genre: genres.COMEDY }
-                ]
-            }
-        },
-        props: [ 'genre', 'time' ],
+        props: [ 'genre', 'time', 'movies' ],
         methods: {
             moviePassesGenreFilter(movie) {
                 if (!this.genre.length) {
