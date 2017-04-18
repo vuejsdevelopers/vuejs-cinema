291d28b09d8f78f5e2afdea0aaeaafd06ef14ba8
Anthony Gore <anthony@magnum.technology>
2017-04-18 14:31:55 +0700

    no message

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -14,7 +14,14 @@
                 if (!this.genre.length) {
                     return true;
                 } else {
-                    return this.genre.find(genre => movie.genre === genre);
+                    let movieGenres = movie.movie.Genre.split(", ");
+                    let matched = true;
+                    this.genre.forEach(genre => {
+                        if (movieGenres.indexOf(genre) === -1) {
+                            matched = false;
+                        }
+                    });
+                    return matched;
                 }
             }
         },
