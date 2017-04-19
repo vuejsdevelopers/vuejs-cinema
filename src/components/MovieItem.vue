e98a133ee34218141f1bff9cb123b25c08ae7ead
Anthony Gore <anthony@magnum.technology>
2017-04-19 16:55:56 +0700

    no message

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -5,7 +5,9 @@
         </div>
         <div class="movie-col-right">
             <div class="movie-title">
-                <h2>{{ movie.Title }}</h2>
+                <router-link v-bind:to="{ name: 'movie' }">
+                    <h2>{{ movie.Title }}</h2>
+                </router-link>
                 <span class="movie-rating">{{ movie.Rated }}</span>
             </div>
             <div class="movie-sessions">
--- src/util/routes.js
+++ src/util/routes.js
@@ -1,5 +1,8 @@
 import Overview from '../components/Overview.vue';
+import Detail from '../components/Detail.vue';
 
 export default [
-  { path: '/', component: Overview }
+  { path: '/', component: Overview, name: 'home' },
+  { path: '/movie', component: Detail, name: 'movie' },
+  { path: '*', redirect: { name: 'home' } }
 ];
