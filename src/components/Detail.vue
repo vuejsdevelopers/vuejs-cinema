56e7d417c2bf752eddb03ece68de24c1da97ab84
Anthony Gore <anthony@magnum.technology>
2017-04-20 16:03:43 +0700

    no message

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,10 +1,21 @@
 <template>
-    <div>Hello World</div>
+    <div v-if="movie" id="detail">
+        <movie-item v-bind:movie="movie.movie" v-bind:sessions="[]"></movie-item>
+    </div>
 </template>
 <script>
+    import MovieItem from './MovieItem.vue';
+
     export default {
-        created() {
-            console.log(this.$route.params.id);
+        props: [ 'movies' ],
+        computed: {
+            movie() {
+                let movie = this.movies.find(movie => movie.id === this.$route.params.id);
+                return movie ? movie : null;
+            }
+        },
+        components: {
+            MovieItem
         }
     }
 </script>
