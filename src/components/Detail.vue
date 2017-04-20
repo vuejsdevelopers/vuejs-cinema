268a0e253cf27b532661dd6485371ce2584abaf4
Anthony Gore <anthony@magnum.technology>
2017-04-20 19:56:47 +0700

    no message

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,6 +1,20 @@
 <template>
     <div v-if="movie" id="detail">
-        <movie-item v-bind:movie="movie.movie"></movie-item>
+        <movie-item v-bind:movie="movie">
+            <div class="movie-details">
+                <p class="movie-genre">{{ movie.Genre }}</p>
+                <p class="movie-plot">{{ movie.Plot }}</p>
+                <table>
+                    <tr><td>Released date: </td><td>{{ movie.Released }}</td></tr>
+                    <tr><td>Running time: </td><td>{{ movie.Runtime }}</td></tr>
+                    <tr><td>Director: </td><td>{{ movie.Director }}</td></tr>
+                    <tr><td>Cast: </td><td>{{ movie.Actors }}</td></tr>
+                </table>
+            </div>
+        </movie-item>
+        <div class="home">
+            <router-link v-bind:to="{ name: 'home' }">Back to results</router-link>
+        </div>
     </div>
 </template>
 <script>
@@ -11,7 +25,7 @@
         computed: {
             movie() {
                 let movie = this.movies.find(movie => movie.id === this.$route.params.id);
-                return movie ? movie : null;
+                return movie ? movie.movie : null;
             }
         },
         components: {
