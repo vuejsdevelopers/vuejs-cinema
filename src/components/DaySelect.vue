512a82e21ecefacfd164efeecf69b28ff455edd1
Anthony Gore <anthony@magnum.technology>
2017-04-21 11:31:12 +0700

    no message

---
+++ src/components/DaySelect.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="day-select">
+        Hello World
+    </div>
+</template>
--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="overview">
+        <day-select></day-select>
         <div class="main">
             <movie-list v-bind:genre="genre" v-bind:time="time" v-bind:movies="movies" v-bind:day="day"></movie-list>
             <movie-filter></movie-filter>
@@ -9,12 +10,14 @@
 <script>
     import MovieList from './MovieList.vue';
     import MovieFilter from './MovieFilter.vue';
+    import DaySelect from './DaySelect.vue';
 
     export default {
         props: [ 'genre', 'time', 'movies', 'day' ],
         components: {
             MovieList,
-            MovieFilter
+            MovieFilter,
+            DaySelect
         },
     }
 </script>
