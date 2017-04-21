05ee83138ce8e60d4b4f00d4632bdd41d12dfc03
Anthony Gore <anthony@magnum.technology>
2017-04-21 11:56:50 +0700

    no message

--- src/components/DaySelect.vue
+++ src/components/DaySelect.vue
@@ -1,5 +1,26 @@
 <template>
     <div id="day-select">
-        Hello World
+        <ul class="days">
+            <li class="day" v-for="day in days">{{ formatDay(day) }}</li>
+        </ul>
     </div>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                selected: this.$moment(),
+                days: [0, 1, 2, 3, 4, 5, 6 ].map(num => this.$moment().add(num, 'days'))
+            };
+        },
+        methods: {
+            formatDay(raw) {
+                if (raw.isSame(this.$moment(), 'day')) {
+                    return 'Today';
+                } else {
+                    return raw.format('ddd DD/MM');
+                }
+            }
+        }
+    }
+</script>
