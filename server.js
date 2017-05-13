136ce66157f3785f175b9546f1e608edae38e5ae
Anthony Gore <anthony@magnum.technology>
2017-05-13 11:04:08 +0700

    Updating error message

--- server.js
+++ server.js
@@ -33,7 +33,11 @@ app.get('/api', function(req, res) {
 
 let offlineData = JSON.parse(fs.readFileSync(path.resolve('./api_offline.json'), 'utf-8'));
 app.get('/offline_api', function(req, res) {
-  res.json(offlineData.find(item => item.imdbID === req.query.i));
+  let data = offlineData.find(item => item.imdbID === req.query.i);
+  if (!data) {
+    data = { "Response":"False", "Error":`IMDb ID ${req.query.i} not found.` }
+  }
+  res.json(data);
 });
 
 app.listen(process.env.PORT, function () {
