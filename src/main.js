9c6d251bf335310376679fcf432d72a6bed7b00f
Tertius Scheepers <Tertiussc@gmail.com>
2019-06-07 12:47:50 +0200

    Added movie title and filter components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,47 @@
 import Vue from 'vue';
-import './style.scss'
+import './style.scss';
+import genres from './util/genres'
 
 new Vue({
-    el: '#app',
-    data() {
-        return {
-            msg: 'Hello World'
-        }
-    },
+el: '#app',
+components: {
+'movie-list': {
+template: `<div id="movie-list">
+    <div class="movie" v-for="movie in movies">{{movie.title}}</div>
+</div>`,
+data() {
+return {
+movies: [
+{ title: 'Pulp Fiction' },
+{ title: 'Home Alone' },
+{ title: 'Austin Powers' },
+]
+}
+},
+},
+'movie-filter': {
+data() {
+return {
+genres
+}
+},
+template: `<div id="movie-filter">
+    <h2>Filter results</h2>
+    <div class="filter-group">
+        <check-filter v-for="genre in genres" :title="genre"></check-filter>
+    </div>
+</div>`,
+components: {
+'check-filter': {
+props: [
+'title'
+],
+template: `<div class="check-filter">
+    <span class="checkbox"></span>
+    <span class="check-filter-title">{{title}}</span>
+</div>`
+}
+}
+}
+},
 })
