e183e6fe179cf5d698f30a57eb4506cfe242be7f
Tertius Scheepers <Tertiussc@gmail.com>
2019-06-07 13:01:55 +0200

    added checked css capability

--- src/main.js
+++ src/main.js
@@ -3,45 +3,50 @@ import './style.scss';
 import genres from './util/genres'
 
 new Vue({
-el: '#app',
-components: {
-'movie-list': {
-template: `<div id="movie-list">
+    el: '#app',
+    components: {
+        'movie-list': {
+            template: `<div id="movie-list">
     <div class="movie" v-for="movie in movies">{{movie.title}}</div>
 </div>`,
-data() {
-return {
-movies: [
-{ title: 'Pulp Fiction' },
-{ title: 'Home Alone' },
-{ title: 'Austin Powers' },
-]
-}
-},
-},
-'movie-filter': {
-data() {
-return {
-genres
-}
-},
-template: `<div id="movie-filter">
+            data() {
+                return {
+                    movies: [
+                        { title: 'Pulp Fiction' },
+                        { title: 'Home Alone' },
+                        { title: 'Austin Powers' },
+                    ]
+                }
+            },
+        },
+        'movie-filter': {
+            data() {
+                return {
+                    genres
+                }
+            },
+            template: `<div id="movie-filter">
     <h2>Filter results</h2>
     <div class="filter-group">
         <check-filter v-for="genre in genres" :title="genre"></check-filter>
     </div>
 </div>`,
-components: {
-'check-filter': {
-props: [
-'title'
-],
-template: `<div class="check-filter">
+            components: {
+                'check-filter': {
+                    data() {
+                        return {
+                            checked: false
+                        }
+                    },
+                    props: [
+                        'title'
+                    ],
+                    template: `<div :class="{'check-filter': true, active: checked}" @click="checked = !checked">
     <span class="checkbox"></span>
     <span class="check-filter-title">{{title}}</span>
 </div>`
-}
-}
-}
-},
+                }
+            }
+        }
+    },
 })
